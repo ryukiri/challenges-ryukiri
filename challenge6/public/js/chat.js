9f2d736f6fd56dd00225af6127262c95fb6480c9
Austin Quach <austinquachh@gmail.com>
2017-11-05 19:49:53 -0800

    stylized login page

--- challenge6/public/js/chat.js
+++ challenge6/public/js/chat.js
@@ -6,3 +6,105 @@
  * 4. Allow a user to log out.
  * 5. Redirect a user to index.html if they are not logged in.
  */
+
+var logoutButton = document.getElementById('logout');
+var auth = firebase.auth();
+var database = firebase.database();
+
+ logoutButton.addEventListener('click', function (e) {
+    console.log(e);
+
+    auth.signOut();
+ });
+
+var messagesList = document.getElementById("messages");
+var messageForm = document.getElementById("message-form");
+var messageInput = document.getElementById("message-input");
+
+auth.onAuthStateChanged(function(user) {
+    // If the user is logged in, user will be an object (truthy).
+    // Otherwise, it will be null (falsey).
+    if (user) {
+        // Logged in
+
+        // Code to get and set values in the database
+
+        // var testRef = database.ref('test2/test3');
+        // testRef.set({
+        //     property: 'Value'
+        // })
+
+        // var testRef = database.ref('test2/test3');
+        // testRef.on('value', function(snapshot) {
+        //     var val = snapshot.val();
+
+        //     console.log(val);
+        // });
+
+        var messages = database.ref('messages');
+
+        // This event listener will be called for each item
+        // that has been added to the list.
+        // Use this to generate each chat message,
+        // both on initial page load and whenver someone creates a new message.
+        messages.on('child_added', function(data) {
+            var id = data.key;
+            var message = data.val();
+
+            var text = message.text;
+            var timestamp = message.timestamp;
+
+            var messageLi = document.createElement('li');
+            messageLi.id = id;
+            messageLi.innerText = text;
+
+            messagesList.appendChild(messageLi);
+        });
+
+        // This event listener will be called whenever an item in the list is edited.
+        // Use this to update the HTML of the message that was edited.
+        messages.on('child_changed', function(data) {
+            var id = data.key;
+            var message = data.val();
+
+        });
+
+        // This event listener will be called whenever an item in the list is deleted.
+        // Use this to remove the HTML of the message that was deleted.
+        messages.on('child_removed', function(data) {
+            var id = data.key;
+
+        });
+        
+    } else {
+        // Logged out
+        window.location.href = 'index.html';
+    }
+});
+
+// When the user submits the form to send a message,
+// add the message to the list of messages.
+messageForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Connect to the firebase data
+    var database = firebase.database();
+
+    // Get the ref for your messages list
+    var messages = database.ref('messages');
+
+    // Get the message the user entered
+    var message = messageInput.value;
+
+    // Create a new message and add it to the list.
+    messages.push({
+        text: message,
+        timestamp: new Date().getTime() // unix timestamp in milliseconds
+    })
+    .then(function () {
+        // message created succesfully
+    })
+    .catch(function(error) {
+        // message not created succesfully
+    });
+});
--- challenge6/public/js/login.js
+++ challenge6/public/js/login.js
@@ -4,3 +4,80 @@
  * 2. Sign in an existing account.
  * 3. Redirect a user to chat.html once they are logged in/signed up.
  */
+
+ var formLogin = document.getElementById('login-form');
+ var emailLogin = document.getElementById('login-inputEmail');
+ var passwordLogin = document.getElementById('login-inputPassword');
+
+ var auth = firebase.auth();
+
+ formLogin.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var email = emailLogin.value;
+    var password = passwordLogin.value;
+
+    console.log(email);
+    console.log(password);
+
+    //Display Loading Bar
+    //loadingBar.style.display = 'block';
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then(function (user) {
+            //loadingBar.style.display = "hidden";
+            console.log(user);
+            console.log("Logged in successfully.");
+        })
+
+        .catch(function (error) {
+            console.log(error.message);
+        });
+ });
+
+// Signup Stuff
+var signupForm = document.getElementById('signup-form');
+var displayNameInput = document.getElementById('signup-displayname');
+var signupEmailInput = document.getElementById('signup-InputEmail');
+var signupPasswordInput = document.getElementById('signup-InputPassword');
+var signupConfirmPasswordInput = document.getElementById('signup-InputPasswordConfirm');
+
+var isSigningUp = false;
+
+signupForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    isSigningUp = true;
+
+    var email = signupEmailInput.value;
+    var password = signupPasswordInput.value;
+
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function (user) {
+        console.log(user);
+
+        // Set display name and photo url
+
+        // Send verificatino email
+        console.log('send email');
+
+        // Redirect
+
+        //window.location.href = 'chat.html';        
+    })
+    .catch(function (e) {
+        // Display error messages
+        console.log(error.message);
+    });
+});
+
+auth.onAuthStateChanged(function(user) {
+    if (user && !isSigningUp) {
+        //User is logged in
+        console.log("Logged in.");
+        window.location.href = 'chat.html';
+    } else {
+        //User is not logged in
+        console.log("Logged out.");
+    }
+});
